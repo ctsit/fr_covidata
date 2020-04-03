faadba4d3674bb1be26a104263a9bfe7011af0f4
Kyle Chesney <kyle.2493@gmail.com>
2020-04-03 14:18:12 -0400

    do not allow next-day appointments after 4pm in dynamic appointment SQL

--- example/dynamic_sql_query_for_appointment.sql
+++ example/dynamic_sql_query_for_appointment.sql
@@ -3,6 +3,8 @@ SELECT a.id, CONCAT(b.site_short_name, ' - ', from_unixtime(a.appointment_block_
         (SELECT * FROM redcap_entity_fr_appointment
             WHERE record_id IS NULL
             AND project_id = [project-id]
+            -- If it is later than 4pm, only show appointments at least 2 days from today
+            AND appointment_block_date > UNIX_TIMESTAMP( DATE( NOW() + INTERVAL IF(HOUR(NOW()) >= 16, 2, 1) DAY ) )
             ORDER BY appointment_block_date
         ) as a
     INNER JOIN redcap_entity_test_site as b ON a.site = b.id
